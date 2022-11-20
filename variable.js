eef27b2b2dec44603e454999c5c12f84508b63ca
FontanaRoman <117094474+FontanaRoman@users.noreply.github.com>
2022-11-20 13:58:14 -0300

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,11 @@
+let pais = "Argentina ";
+let continente = "Latinoamerica ";
+let antiguedad = "2022"; 
+let pais_y_continente = pais + " " + continente + " " + antiguedad;
+
+pais = "Alemania ";
+continente = "Europa ";
+
+console.log(pais, continente, antiguedad);
+alert(pais_y_continente);
+document.write(pais, continente);
